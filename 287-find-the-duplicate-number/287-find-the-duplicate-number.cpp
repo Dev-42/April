a5c6a23a0711509c6974614fd5a193e7bdc7a1aa
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-04-12 09:23:35 +0530

    Time: 88 ms (99.08%), Space: 61.3 MB (26.61%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,10 +2,21 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) 
     {
-       while(nums[0]!=nums[nums[0]])
-       {
-           swap(nums[0],nums[nums[0]]);
-       }
-        return nums[0];
+        //Let's Use the most optimised technique to solve this problem
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        do{
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        }while(tortoise != hare);
+        
+        hare = nums[0];
+        while(tortoise != hare)
+        {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return hare;
     }
 };
