bc7176f15943c5040e91c4ccf3328893fb3014ef
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-05-28 10:32:20 +0530

    Time: 38 ms (20.53%), Space: 17.9 MB (65.37%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int totN = (n*(n+1)/2);
+        int total = 0;
+        for(int i=0;i<n;i++)
+        {
+            total+=nums[i];
+        }
+        return totN - total;
+    }
+};
