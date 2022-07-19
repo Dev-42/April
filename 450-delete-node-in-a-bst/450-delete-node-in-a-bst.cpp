1d4296a7a2337efe74bf345b2a7b207062c77375
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-07-19 11:27:10 +0530

    Time: 40 ms (84.94%), Space: 32.7 MB (78.28%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) 
+    {
+        if(root == NULL)
+            return NULL;
+        else if(root->val==key)
+            return helper(root);
+        TreeNode* dummy = root;
+        while(root != NULL)
+        {
+            if(root->val > key)
+            {
+                if(root->left != NULL && root->left->val == key)
+                {
+                    root->left = helper(root->left);
+                    break;
+                }
+                else{
+                    root = root->left;
+                }
+            }
+            else{
+                if(root->right != NULL && root->right->val == key)
+                {
+                    root->right = helper(root->right);
+                    break;
+                }
+                else{
+                    root = root->right;
+                }
+            }
+        }
+        return dummy;
+    }
+    
+    TreeNode* helper(TreeNode* root)
+    {
+        if(root->left == NULL)
+            return root->right;
+        else if(root->right == NULL)
+            return root->left;
+        
+        TreeNode* rootRight = root->right;
+        TreeNode* lastRight = findlastRight(root->left); 
+        lastRight -> right = rootRight;
+        return root->left;
+    }
+    TreeNode* findlastRight(TreeNode* root)
+    {
+        if(root-> right == NULL)
+            return root;
+        return findlastRight(root->right);
+    }
+};
