7fdda49088f07ddbaaca24e22704c79a204f05fc
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-08-15 22:07:16 +0530

    Time: 99 ms (96.27%), Space: 50.1 MB (51.90%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.js
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.js
@@ -1,28 +1,26 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-class Solution {
-public:
-    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) 
-    {
-        if(root1 == NULL)
-            return root2;
-        if(root2 == NULL)
-            return root1;
-        
-        root1 -> val += root2 -> val;
-        
-        root1 -> left = mergeTrees(root1 -> left , root2 -> left);
-        root1 -> right = mergeTrees(root1 -> right , root2 -> right);
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+let mergeTrees = function(root1, root2) {
+    if(root1 == null)
+        return root2;
+    if(root2 == null)
         return root1;
-        
-    }
+    
+    //doing the main operation
+    root1.val +=root2.val;
+    
+    root1.left = mergeTrees(root1.left,root2.left);
+    root1.right = mergeTrees(root1.right,root2.right);
+    return root1;
 };
