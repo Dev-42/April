531f9727ad6082cc5918b369b09d2219d0f0dc15
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-08-24 09:02:14 +0530

    Time: 174 ms (78.10%), Space: 69.8 MB (78.41%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -1,35 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int goodNodes(TreeNode* root) 
+var goodNodes = function(root) {
+    let count = 0
+    
+    const dfs = (node, maxValue) => 
     {
-        int count = 0;
-        if(root == NULL)
-            return -1;
-        countGood(root , count , root -> val);
-        return count;
+        if (node.val >= maxValue) 
+        { 
+            count++ 
+            maxValue = node.val
+        }
+        if (node.left) dfs(node.left, maxValue)
+        if (node.right) dfs(node.right, maxValue)
     }
     
-    void countGood(TreeNode* root,int &count,int maxValue)
-    {
-        if(!root)
-            return;
-        if(root -> val >= maxValue)
-        {
-            count += 1;
-            maxValue = root -> val;
-        }
-        countGood(root -> left,count,maxValue);
-        countGood(root -> right,count,maxValue);
-    }
+    dfs(root, root.val)
+
+    
+    return count
 };
